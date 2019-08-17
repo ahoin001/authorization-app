463305610fcc71063d10a26c734229cc429619fe
Alexander Hoinville <ahoin001@gmail.com>
2019-08-16 21:52:35 -0400

    Added username validation and password validation

--- basic-auth/routes/auth.js
+++ basic-auth/routes/auth.js
@@ -12,37 +12,60 @@ router.get("/signup", (req, res, next) => {
 // BCRYPT TO ENCRYPTION PASSWORDS
 //*********************************************************** */
 
-const bcrypt     = require("bcrypt");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 // will be redirected to same page after form is submitted and user will be added to Collection of users
-router.post('/signup', (req,res,next) => {
+router.post('/signup', (req, res, next) => {
     const username = req.body.username;
     const password = req.body.password;
     const salt = bcrypt.genSaltSync(saltRounds);
-    const hashPassword = bcrypt.hashSync(password,salt)
-  
+    const hashPassword = bcrypt.hashSync(password, salt)
+
+    // VALIDATES USERNAME AND PASSWORDS FIELDS TO MAKE SURE THEY ARE NOT EMPTY
+    if (username === "" || password === "") {
+        // if user or password empty, send back to same page, notify them of error with error 
+        // second parameter is object passed to hbs to display the message
+        res.render('auth/signup', {
+            errorMessage: "Username and password must both not be empty."
+        })
+        // breaks us out of function before creating new user
+        return;
+    }
+
+    // CHECKS IF USERNAME ALREADY EXSISTS IN THE DB COLLECTION WE HAVE
+    User.findOne({ "username": username })
+        .then(user => {
+            if (user !== null) {
+                res.render("auth/signup", {
+                    errorMessage: "The username already exists!"
+                });
+                return;
+            }
+        })
+
+    // IF ALL VALIDATION PASSED THEN CREATE NEW USER
     User
-    .create({
-  
-      username,
-      // pass the hashed password to password property
-      password : hashPassword 
-  
-    })
-    .then( () => {
-      // Show if user is created in console
-      User
-      .find()
-      .then((users) => {
-        console.log('LIST OF USERS ------------',users);
-      }
-      )
-      res.redirect('/');
-    })
-    .catch((err) => {
-      console.log('Error redirecting to root page with user information ============================');
-    })
-  })
+        .create({
+
+            username,
+            // pass the hashed password to password property
+            password: hashPassword
+
+        })
+        .then(() => {
+            // Show if user is created in console
+            User
+                .find()
+                .then((users) => {
+                    console.log('LIST OF USERS ------------', users);
+                }
+                )
+            res.redirect('/');
+        })
+        .catch((err) => {
+            console.log('Error redirecting to root page with user information ============================');
+        })
+})
 
 module.exports = router;
