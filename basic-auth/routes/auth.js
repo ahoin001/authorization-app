6575914d300f80669658296ff9ad63b4e782130f
Alexander Hoinville <ahoin001@gmail.com>
2019-08-16 20:43:46 -0400

    Create root index page with link to pages

--- basic-auth/routes/auth.js
+++ basic-auth/routes/auth.js
@@ -1,9 +1,11 @@
 
 const express = require("express");
+// User model
+const User = require("../models/user")
 const router = express.Router();
 
 router.get("/signup", (req, res, next) => {
-  res.render("auth/signup");
+    res.render("auth/signup");
 });
 
 module.exports = router;
